b3f9eb9208311698ac89d84df496f2347e03e795
KakaChristian <christ123461@gmail.com>
2024-04-02 11:06:04 +0800

    Menambahkan Rumus Luas Persegi Panjang

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ alas = float(input("Inpus Alas Segitiga : "))
 tinggi = float(input("Inpus Tinggi Segitiga : "))
 print('Luas Segitiga =', hitungpLuasSegitiga(alas, tinggi))
 print()
+
+print("## PROGRAM PYTHON MENGHITUNG LUAS SEGITIGA ##")
+print("=============================================")
+print()
+
+def hitungPersegiPanjang(p, l):
+    return round(p * l,2)
+
+panjang = float(input("Input Panjang : "))
+Lebar = float(input("Input Lebar : "))
+print("Luas Persegi Panjang = ", hitungpLuasSegitiga(panjang, Lebar))
